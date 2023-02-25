49605695961632a3f754d16a2c3594d78835abfb
egleg <egle.gdv@gmail.com>
2023-02-25 09:49:35 +0200

    Add new class BaseTest

---
+++ src/test/java/lt/egle/keramikams/pom/tests/common/BaseTest.java
@@ -0,0 +1,19 @@
+package lt.egle.keramikams.pom.tests.common;
+
+import lt.egle.keramikams.pom.pages.Common;
+import lt.egle.keramikams.pom.utils.TestListener;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+
+@Listeners(TestListener.class)
+public abstract class BaseTest {
+
+    @BeforeMethod
+    public abstract void setup();
+
+    @AfterMethod
+    public void teardown() {
+        Common.closeDriver();
+    }
+}
