b87a8721ffe7c7179de785ebc04abfcc8f4e8d9a
egleg <egle.gdv@gmail.com>
2023-02-25 09:49:22 +0200

    Add new class Common

---
+++ src/test/java/lt/egle/keramikams/pom/pages/Common.java
@@ -0,0 +1,90 @@
+package lt.egle.keramikams.pom.pages;
+
+import lt.egle.keramikams.pom.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Common {
+    public static void openUrl(String url) {
+        Driver.setDriver();
+        Driver.getDriver().get(url);
+    }
+
+    public static void closeDriver() {
+        Driver.closeDriver();
+    }
+
+    private static WebElement getElement(By locator) {
+        return Driver.getDriver().findElement(locator);
+    }
+
+    public static void sendKeysToElement(By locator, String text) {
+        getElement(locator).sendKeys(text);
+    }
+
+    public static void clickElement(By locator) {
+        getElement(locator).click();
+    }
+
+    public static String getElementText(By locator) {
+        return getElement(locator).getText();
+    }
+
+    public static void selectOptionByValue(By locator, String day) {
+        WebElement element = getElement(locator);
+        Select select = new Select(element);
+        select.selectByValue(day);
+    }
+
+    public static List<Boolean> getSelectedStatusOfAllCheckboxes(By locator) {
+        List<WebElement> elements = getElements(locator);
+        List<Boolean> elementsStatuses = new ArrayList<>();
+
+        for (WebElement element : elements) {
+            elementsStatuses.add(element.isSelected());
+        }
+        return elementsStatuses;
+    }
+
+    private static List<WebElement> getElements(By locator) {
+        return Driver.getDriver().findElements(locator);
+
+    }
+
+    public static String getElementAttributeOutcome(By locator, String attributeName) {
+        return getElement(locator).getAttribute(attributeName);
+    }
+
+    public static void acceptAlert() {
+        Driver.getDriver().switchTo().alert().accept(); //JavaScript lentelei uzdaryti
+    }
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static boolean isAlertPresent() {
+        try {
+            Driver.getDriver().switchTo().alert();
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void dismissAlert() {
+        Driver.getDriver().switchTo().alert().dismiss();
+    }
+
+}
