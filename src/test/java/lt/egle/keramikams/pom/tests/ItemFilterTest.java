f431e57fb1869c848cb993990020a5bb02de4f59
egleg <egle.gdv@gmail.com>
2023-02-26 09:52:24 +0200

    Add new class ItemFilterTest

---
+++ src/test/java/lt/egle/keramikams/pom/tests/ItemFilterTest.java
@@ -0,0 +1,14 @@
+package lt.egle.keramikams.pom.tests;
+
+import lt.egle.keramikams.pom.tests.common.BaseTest;
+import org.testng.annotations.BeforeMethod;
+
+public class ItemFilterTest extends BaseTest {
+
+
+    @BeforeMethod
+    @Override
+    public void setup() {
+
+    }
+}
