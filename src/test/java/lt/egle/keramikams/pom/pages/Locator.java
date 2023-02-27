ac8205b2405c737fcbe4c5abcdb267afb909ed17
egleg <egle.gdv@gmail.com>
2023-02-27 13:39:43 +0200

    Edited class Locator

--- src/test/java/lt/egle/keramikams/pom/pages/Locator.java
+++ src/test/java/lt/egle/keramikams/pom/pages/Locator.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 public class Locator {
 
 
-    public class Login {
+    public static class Login {
 
         public static By wordClickablePrisijungti = By.xpath("(//a[contains(text(),'Prisijungti')])[1]");
         public static By fieldElPastas = By.xpath("//input[@name='login[username]']");
@@ -15,12 +15,12 @@ public class Locator {
         public static By errorMessage = By.xpath("//div[contains(text(),'Neteisingas prisijungimo vardas arba slaptažodis.')]");
     }
 
-    public class CommonActions {
+    public static class CommonActions {
         public static By buttonCookiesSutinku = By.xpath("//span[contains(text(),'Sutinku')]");
         public static By buttonNuorodos = By.xpath("//*[contains(text(),'Nuorodos')]");
     }
 
-    public class ItemSearch {
+    public static class ItemSearch {
 
         public static By inputSearch = By.xpath("//input[@id='search']");
         public static By iconSearch = By.xpath("//button[@class='action search']");
@@ -28,7 +28,7 @@ public class Locator {
         public static By textMessageNotice = By.xpath("//div[@class='message notice']");
     }
 
-    public class AddToCart {
+    public static class AddToCart {
 
         public static By buttonIKrepseliRaudonaRiebiMolioMase = By.xpath("//form[@action='https://keramikams.lt/checkout/cart/add/uenc/aHR0cHM6Ly9rZXJhbWlrYW1zLmx0L2NhdGFsb2dzZWFyY2gvcmVzdWx0Lz9xPVJhdWRvbmErcmllYmkrbW9saW8rbWFzJUM0JTk3Kw%2C%2C/product/5551/']");
         public static By fieldQuantity20Kg = By.xpath("//input[@name='super_group[2180]']");
@@ -37,7 +37,7 @@ public class Locator {
         public static By numberofItemsinBasket = By.xpath("//span[@class='counter-number']");
     }
 
-    public class ItemFilter {
+    public static class ItemFilter {
         public static By spanKaina = By.xpath("//span[@data-price-type='finalPrice']");
         public static By buttonRikiavimas = By.xpath("(//select[@id='sorter'])[1]");
         public static By arrowSetDescending = By.xpath("(//a[@title='Set Descending Direction'])[1]");
